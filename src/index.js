6aca943aafeb3d6a5f8ee37334af2fb66e79ebf6
kobi <kobiokiti@gmail.com>
2025-11-24 20:03:35 +0100

    develoepd the patch and delete routes, to update and delete a note

--- src/index.js
+++ src/index.js
@@ -4,25 +4,58 @@ const fs = require("fs");
 require("./db/mongoose");
 
 const Note = require("./models/note");
+const { NOTINITIALIZED } = require("dns");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/notes", async (req, res) => {
-  try {
-    const notes = await Note.find({});
-    res.send(notes)
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+// CRUD
 
+// CREATE
 app.post("/notes", async (req, res) => {
   const note = new Note(req.body);
   try {
     await note.save();
     res.status(200).send(note);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// READ
+app.get("/notes", async (req, res) => {
+  try {
+    const notes = await Note.find({});
+    res.send(notes);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// UPDATE
+app.patch("/notes/:id", async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    if(!note) {
+        return res.status(404).send()
+    }
+    note.note = req.body.note;
+    await note.save();
+    res.status(200).send(note);
+  } catch (err) {
+    res.status(404).send(err);
+  }
+});
+
+// DELETE
+app.delete("/notes/:id", async (req, res) => {
+  try {
+    const note = await Note.findByIdAndDelete(req.params.id);
+    if(!note) {
+        return res.status(404).send()
+    }
+    res.status(200).send("The Note has been deleted");
   } catch (err) {
     res.status(500).send(err);
   }
