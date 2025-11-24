a71109fdd75584bf5bf0636bde96dd6922094947
kobi <kobiokiti@gmail.com>
2025-11-24 19:15:51 +0100

    created the route for the Get request foe notes

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
-const express = require('express');
-const fs = require('fs')
+const express = require("express");
+const fs = require("fs");
 
-require('./db/mongoose')
+require("./db/mongoose");
 
-const Note = require('./models/note')
+const Note = require("./models/note");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/notes', (req, res) => {
-    fs.readFile(__dirname + '/' + 'notes.json' , 'utf-8', (err, data) => {
-        if (err) {
-            return console.log(err)
-        }
+app.get("/notes", async (req, res) => {
+  try {
+    const notes = await Note.find({});
+    res.send(notes)
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
-        res.send(data)
-    })
-})
-
-app.post('/notes', (req, res) => {
-    const note = new Note(req.body)
-    note.save()
-    .then(() => {
-        res.status(200).send(note)
-    })
-    .catch((err) => {
-        res.status(500).send(err)
-    })
-})
+app.post("/notes", async (req, res) => {
+  const note = new Note(req.body);
+  try {
+    await note.save();
+    res.status(200).send(note);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 app.listen(3000, () => {
-    console.log("Server is up on port 3000")
-})
+  console.log("Server is up on port 3000");
+});
